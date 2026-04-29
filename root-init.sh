aeee96ccf0b4544ee729c94899194a7a9de51e12
venssy <huangvenssy@163.com>
2026-04-30 07:39:40 +0800

    Fix symbolic link direction for OpenClaw config

--- root-init.sh
+++ root-init.sh
@@ -170,7 +170,7 @@ wait_for_gateway() {
 
 main() {
     log_section "OpenClaw start"
-    ln -s /root/.config $OPENCLAW_HOME/.config
+    ln -s $OPENCLAW_HOME/.config /root/.config
     
     setup_runtime_env
     install_agent_reach
